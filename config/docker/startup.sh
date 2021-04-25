bf772dfe10cfe160f34cbe690b20d3d7eefcabf5
Tales Mousine <tazmouzine@gmail.com>
2021-04-25 15:11:59 -0300

    Fix: statup

--- config/docker/startup.sh
+++ config/docker/startup.sh
@@ -9,5 +9,8 @@ sh ./config/docker/prepare-db.sh
 # Pre-comple app assets
 sh ./config/docker/asset-pre-compile.sh
 
+# Bootsnap precompile
+bundle exec bootsnap precompile --gemfile app/ lib/
+
 # Start Application
 bundle exec puma -C config/puma.rb
